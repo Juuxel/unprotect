0734c25f455164411b496ec8c11a73cc271c2fff
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-05-03 14:24:42 +0300

    Add back module-info that is compiled separately

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,12 @@ plugins {
 group = "io.github.juuxel"
 version = "0.0.7"
 
+val modularity = sourceSets.register("modularity") {
+    java {
+        srcDirs(sourceSets.main.map { it.java.srcDirs })
+    }
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
@@ -19,28 +25,41 @@ repositories {
 }
 
 dependencies {
+    val modLauncher8 = "8.0.9"
+    val modLauncher9 = "9.0.4"
+
     compileOnly("org.jetbrains:annotations:23.0.0")
-    compileOnly("cpw.mods:modlauncher:8.0.9")
+    compileOnly("cpw.mods:modlauncher:$modLauncher8")
+
+    "modularityCompileOnly"("org.jetbrains:annotations:23.0.0")
+    "modularityCompileOnly"("cpw.mods:modlauncher:$modLauncher9")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("cpw.mods:modlauncher:8.0.9")
+    testImplementation("cpw.mods:modlauncher:$modLauncher8")
 }
 
 tasks {
     withType<JavaCompile> {
         options.encoding = "UTF-8"
+        options.release.set(8)
+    }
 
-        if (JavaVersion.current().isJava9Compatible) {
-            options.release.set(8)
-        }
+    "compileModularityJava"(JavaCompile::class) {
+        options.release.set(16)
     }
 
     jar {
         from("LICENSE")
+        from(modularity.map { it.output }) {
+            include("module-info.class")
+        }
     }
 
     "sourcesJar"(Jar::class) {
         from("LICENSE")
+        from(modularity.map { it.allSource }) {
+            include("module-info.java")
+        }
     }
 
     test {
